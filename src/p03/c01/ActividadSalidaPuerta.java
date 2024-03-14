09cd8f28849e14a6f3e891bf787ad3adac1da95f
javim1993 <jmf1017@alu.ubu.es>
2024-03-14 19:52:05 +0100

    Update ActividadSalidaPuerta.java

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,8 +1,30 @@
 package src.p03.c01;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class ActividadSalidaPuerta{
+public class ActividadSalidaPuerta implements Runnable{
+	private static final int NUMSALIDAS = 20;
+	private String puerta;
+	private IParque parque;
 
-	//
-	// TODO
-	//
+	public ActividadSalidaPuerta(String puerta, IParque parque) {
+		this.puerta = puerta;
+		this.parque = parque;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < NUMSALIDAS; i ++) {
+			try {
+				parque.salirDelParque(puerta);
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
+			} catch (InterruptedException e) {
+				Logger.getGlobal().log(Level.INFO, "Salida interrumpida");
+				Logger.getGlobal().log(Level.INFO, e.toString());
+				return;
+			}
+		}
+	}
 }
