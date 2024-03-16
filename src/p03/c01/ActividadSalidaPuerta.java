dc6197c53b78e91f2b70335f0736229fc379699f
javim1993 <jmf1017@alu.ubu.es>
2024-03-16 12:31:17 +0100

    Update ActividadSalidaPuerta.java

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,21 +1,48 @@
 package src.p03.c01;
+
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ActividadSalidaPuerta implements Runnable{
-	private static final int NUMSALIDAS = 20;
-	private String puerta;
-	private IParque parque;
+/**
+ * Clase ActividadSalidaParque
+ *  
+ * @author <a href="jmf1017@alu.ubu.es">Javier Muñoz</a>
+ * @author <a href="jgh1006@alu.ubu.es">Josue Gabriel Granados</a>
+ * @since 1.0
+ * @version 1.0
+ * 
+ *          Clase ActividadSalidaPuerta que implementa la interfaz Runnable 
+ *          para ejecutar thread con run
+ */
 
+public class ActividadSalidaPuerta implements Runnable{
+	/** Numero máximo de salidas por puerta*/
+	private static final int NUMSALIDAS = 100;
+	/** String que indica la puerta de salida*/
+	private String puerta;
+	/** Objeto de la interfaz Iparque*/
+	private IParque parque;
+	
+	/**
+	 * Metodo Constructor de la clase
+	 * 
+	 * Se le pasan como parámetros:
+	 * 
+	 * @param puerta puerta por la que ejecuta el thread
+	 * @param parque parque de la interfaz IParque
+	 */
 	public ActividadSalidaPuerta(String puerta, IParque parque) {
 		this.puerta = puerta;
 		this.parque = parque;
 	}
-
+	
+	/**
+	 * Metodo run sobreescrito de la interfaz runnable
+	 */
 	@Override
-	public void run() {
+	public void run() {	
 		for (int i = 0; i < NUMSALIDAS; i ++) {
 			try {
 				parque.salirDelParque(puerta);
