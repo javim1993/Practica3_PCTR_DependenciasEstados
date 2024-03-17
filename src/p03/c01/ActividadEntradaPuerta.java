9d66904851ded0073b7709392262033126b3d42b
javim1993 <jmf1017@alu.ubu.es>
2024-03-17 12:31:58 +0100

    Update ActividadEntradaPuerta.java
    Comentarios en clase

--- src/p03/c01/ActividadEntradaPuerta.java
+++ src/p03/c01/ActividadEntradaPuerta.java
@@ -5,30 +5,55 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ActividadEntradaPuerta implements Runnable{
+/**
+ * Clase ActividadEntradaPuerta
+ * 
+ * @author <a href="jmf1017@alu.ubu.es">Javier Muñoz</a>
+ * @author <a href="jgh1006@alu.ubu.es">Josue Gabriel Granados</a>
+ * @since 1.0
+ * @version 1.0
+ * 
+ *          Clase ActividadEntradaPuerta que implementa la interfaz Runnable
+ *          para ejecutar thread con run
+ */
+public class ActividadEntradaPuerta implements Runnable {
 
-		private static final int NUMENTRADAS = 20;
-		private String puerta;
-		private IParque parque;
+	/** Numero máximo de salidas por puerta */
+	private static final int NUMENTRADAS = 20;
+	/** String que indica la puerta de salida */
+	private String puerta;
+	/** Objeto de la interfaz Iparque */
+	private IParque parque;
 
-		public ActividadEntradaPuerta(String puerta, IParque parque) {
-			this.puerta = puerta;
-			this.parque = parque;
-		}
+	/**
+	 * Metodo Constructor de la clase
+	 * 
+	 * Se le pasan como parámetros:
+	 * 
+	 * @param puerta puerta por la que ejecuta el thread
+	 * @param parque parque de la interfaz IParque
+	 */
+	public ActividadEntradaPuerta(String puerta, IParque parque) {
+		this.puerta = puerta;
+		this.parque = parque;
+	}
 
-		@Override
-		public void run() {
-			for (int i = 0; i < NUMENTRADAS; i ++) {
-				try {
-					parque.entrarAlParque(puerta);
-					TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
-					i++;
-				} catch (InterruptedException e) {
-					Logger.getGlobal().log(Level.INFO, "Entrada interrumpida");
-					Logger.getGlobal().log(Level.INFO, e.toString());
-					return;
-				}
+	/**
+	 * Metodo run sobreescrito de la interfaz runnable
+	 */
+	@Override
+	public void run() {
+		for (int i = 0; i < NUMENTRADAS; i++) {
+			try {
+				parque.entrarAlParque(puerta);
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5) * 1000);
+				i++;
+			} catch (InterruptedException e) {
+				Logger.getGlobal().log(Level.INFO, "Entrada interrumpida");
+				Logger.getGlobal().log(Level.INFO, e.toString());
+				return;
 			}
 		}
+	}
 
 }
