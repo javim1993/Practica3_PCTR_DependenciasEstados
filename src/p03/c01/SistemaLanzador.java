e45c71371fe5462ab8c57744af745baa988f91a5
Josuari9 <33183836+josuari9@users.noreply.github.com>
2024-03-18 18:55:16 +0100

    Update SistemaLanzador.java

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -2,14 +2,18 @@ package src.p03.c01;
 
 public class SistemaLanzador {
 	public static void main(String[] args) {
-		
-		IParque parque = new Parque(); // TODO
+		/** Interfaz de tipo IParque */
+		IParque parque = new Parque(); 
+		/** Comienzo de las puertas */
 		char letra_puerta = 'A';
-			
+		/** Máximo de puertas */
+		int MAXPUERTAS = 5;	
 		System.out.println("¡Parque abierto!");
+
 		
+		/** Se limita el número de puertas a 5 */
 		//for (int i = 0; i < Integer.parseInt(args[0]); i++) {
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < MAXPUERTAS; i++) {
 			String puerta = ""+((char) (letra_puerta++));
 			
 			// Creación de hilos de entrada
